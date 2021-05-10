f5cd0e83485612d16a05f52ae8c4318efafa441d
Fabian <fabian@meetball.live>
2021-05-10 20:20:58 +0200

    v0.1.6

--- src/logger.js
+++ src/logger.js
@@ -19,7 +19,7 @@ const loggerWrapper = (absoluteFilePath) => {
   // Because this file is in the source code root folder, the above will make all paths relative to it: just the info needed for the log.
 
   return {
-    info: (message) => logger.info(`[${file}] ${message}`),
+    info: (message, meta) => logger.info(`[${file}] ${message}`, { meta }),
     warn: (message) => logger.warn(`[${file}] ${message}`),
     error: (message, error) =>
       logger.error(
